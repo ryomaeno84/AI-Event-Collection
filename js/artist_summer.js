66785648b62701666c53e10a8fd198ca6560a34c
A S <kanariya.glay84@gmail.com>
2026-05-02 08:17:51 +0900

    summer.htmlに出演者一覧を正式記載

--- js/artist_summer.js
+++ js/artist_summer.js
@@ -1,27 +1,428 @@
 /**
- * Artist Data: Summer LIVE
+ * Artist Data: Summer LIVE 2026 Updated (Full 35 Artists - Updated Image Path)
  */
 
 const summerArtists = [
   {
-    name: "Artist 01",
-    image: "image/artist/summer/icon01.png",
-    xUrl: "https://x.com/username",
-    youtubeUrl: "https://youtube.com/@channel"
+    name: "FelisCatus",
+    image: "image/summer2026/icon/icon01.jpg",
+    xUrl: "https://x.com/felis_catus_89",
+    youtubeUrl: "https://www.youtube.com/@felis_catus_ai"
   },
   {
-    name: "Artist 02",
-    image: "image/artist/summer/icon02.png",
-    xUrl: "#",
+    name: "Project Muse",
+    image: "image/summer2026/icon/icon02.jpg",
+    xUrl: "https://x.com/projectmuse_ai",
+    youtubeUrl: "https://www.youtube.com/@projectmuse_ai"
+  },
+  {
+    name: "豆仙人",
+    image: "image/summer2026/icon/icon03.png",
+    xUrl: "https://x.com/l1fGnD1V5T81625",
+    youtubeUrl: "https://youtube.com/@mame-no-room?si=kPiC3Hr3SnSui8qW"
+  },
+  {
+    name: "さくなな",
+    image: "image/summer2026/icon/icon04.jpg",
+    xUrl: "https://x.com/sakura_nanajo",
+    youtubeUrl: "https://www.youtube.com/@sakura.nanajo"
+  },
+  {
+    name: "To",
+    image: "image/summer2026/icon/icon05.JPG",
+    xUrl: "https://x.com/divarias_suno?s=21&t=ndz2zklhnxycajH0oeV8cw",
+    youtubeUrl: "https://youtube.com/channel/UCQ-_aDu402PryjjsbIyE82g?si=O4Vl6EAWz3oXifCU"
+  },
+  {
+    name: "ハルイロナツイロ",
+    image: "image/summer2026/icon/icon06.jpg",
+    xUrl: "https://x.com/harunatsu_tuber",
+    youtubeUrl: "https://www.youtube.com/@アビセレ"
+  },
+  {
+    name: "SDAI",
+    image: "image/summer2026/icon/icon07.png",
+    xUrl: "https://x.com/SDAI1807097011",
+    youtubeUrl: "https://www.youtube.com/channel/UCqddWBT68j-ut0MLQ956N8w"
+  },
+  {
+    name: "トミオ",
+    image: "image/summer2026/icon/icon08.png",
+    xUrl: "https://x.com/toMion818",
+    youtubeUrl: "https://www.youtube.com/@toMion818"
+  },
+  {
+    name: "からすい",
+    image: "image/summer2026/icon/icon100.png",
+    xUrl: "https://x.com/karasuiAi",
+    youtubeUrl: "https://www.youtube.com/@Lo-fiDetective"
+  },
+  {
+    name: "Cage13",
+    image: "image/summer2026/icon/icon100.png",
+    xUrl: "https://x.com/hirohiroronron",
+    youtubeUrl: "https://www.youtube.com/@cage1333"
+  },
+  {
+    name: "Relation",
+    image: "image/summer2026/icon/icon11.png",
+    xUrl: "https://x.com/RelationSuno",
+    youtubeUrl: "https://www.youtube.com/@Relation-suno"
+  },
+  {
+    name: "MOGU",
+    image: "image/summer2026/icon/icon12.png",
+    xUrl: "https://x.com/guri_maru_OSE",
+    youtubeUrl: "https://www.youtube.com/channel/UCCI1Ss9-p5U4CxU47aGLDHQ"
+  },
+  {
+    name: "Jayz",
+    image: "image/summer2026/icon/icon13.jpg",
+    xUrl: "https://x.com/PElfines71321",
+    youtubeUrl: "https://www.youtube.com/channel/UCy6_jvhI4K_OfHM-IlXn4iQ"
+  },
+  {
+    name: "神楽レイア",
+    image: "image/summer2026/icon/icon14.jpg",
+    xUrl: "https://x.com/kagurareiavv",
+    youtubeUrl: "https://www.youtube.com/channel/UCjUGIWW2q7_yQER0txD24zg"
+  },
+  {
+    name: "YB",
+    image: "image/summer2026/icon/icon15.png",
+    xUrl: "https://x.com/YB35769492",
+    youtubeUrl: "https://www.youtube.com/@ameronanora1111"
+  },
+  {
+    name: "天汐香弓",
+    image: "image/summer2026/icon/icon16.jpg",
+    xUrl: "https://x.com/teshio_k",
+    youtubeUrl: "https://www.youtube.com/@user-usuisioaji"
+  },
+  {
+    name: "kojirou",
+    image: "image/summer2026/icon/icon17.png",
+    xUrl: "https://x.com/kojirou_SRP",
+    youtubeUrl: "https://www.youtube.com/@kojirou-Producer"
+  },
+  {
+    name: "奥州健一",
+    image: "image/summer2026/icon/icon18.png",
+    xUrl: "https://x.com/o_syukniti",
+    youtubeUrl: "https://t.co/cVT6dXIIWX"
+  },
+  {
+    name: "ShadeLurk",
+    image: "image/summer2026/icon/icon19.png",
+    xUrl: "https://x.com/ShadeLurk",
+    youtubeUrl: "https://www.youtube.com/@ShadeLurk2011"
+  },
+  {
+    name: "STN",
+    image: "image/summer2026/icon/icon20.png",
+    xUrl: "https://x.com/stn_aicreator",
+    youtubeUrl: "https://www.youtube.com/@stn_ai"
+  },
+  {
+    name: "ＭＩＸ＠1981JAPAN",
+    image: "image/summer2026/icon/icon21.png",
+    xUrl: "https://x.com/mizudaisuki1981?s=21",
     youtubeUrl: "#"
   },
   {
-    name: "Artist 03",
-    image: "image/artist/summer/icon03.png",
-    xUrl: "#",
+    name: "mzk",
+    image: "image/summer2026/icon/icon22.jpg",
+    xUrl: "https://x.com/mzk_grave",
+    youtubeUrl: "https://www.youtube.com/@HEARTofCyan"
+  },
+  {
+    name: "YURAnoHITORIGOTO",
+    image: "image/summer2026/icon/icon23.png",
+    xUrl: "https://x.com/ynh_yura",
+    youtubeUrl: "https://www.youtube.com/@yuraxkuro_yura"
+  },
+  {
+    name: "創作実験HIRO",
+    image: "image/summer2026/icon/icon24.jpg",
+    xUrl: "https://x.com/SousakujikkenH",
+    youtubeUrl: "https://www.youtube.com/@sousakujikken_hiro"
+  },
+  {
+    name: "ゆうじ",
+    image: "image/summer2026/icon/icon25.jpg",
+    xUrl: "https://x.com/yuji_aiworks",
     youtubeUrl: "#"
+  },
+  {
+    name: "KENJI MUNEDA",
+    image: "image/summer2026/icon/icon26.jpeg",
+    xUrl: "https://x.com/mu_kenji_t",
+    youtubeUrl: "https://www.youtube.com/@mu_kenji_t"
+  },
+  {
+    name: "五島 雅",
+    image: "image/summer2026/icon/icon27.jpg",
+    xUrl: "https://x.com/miyabigoshima",
+    youtubeUrl: "https://www.youtube.com/@MIYABI_GOSHIMA"
+  },
+  {
+    name: "ぱんちょ",
+    image: "image/summer2026/icon/icon28.jpg",
+    xUrl: "https://x.com/pancyo999",
+    youtubeUrl: "https://youtube.com/@pancyo924?si=G-lOZHlMgc-kfteL"
+  },
+  {
+    name: "Katsunn",
+    image: "image/summer2026/icon/icon29.jpg",
+    xUrl: "https://x.com/ka2nnai_nekoog",
+    youtubeUrl: "https://www.youtube.com/@NekoOtomeCH"
+  },
+  {
+    name: "Hina",
+    image: "image/summer2026/icon/icon30.jpg",
+    xUrl: "https://x.com/hina23x",
+    youtubeUrl: "https://www.youtube.com/@LuminaNavis"
+  },
+  {
+    name: "藍野シアン",
+    image: "image/summer2026/icon/icon31.jpg",
+    xUrl: "https://x.com/ugus9GSDC6j9nC8",
+    youtubeUrl: "https://www.youtube.com/@AINO_Channel_Cyan"
+  },
+  {
+    name: "終電ダッシュヒーローズ",
+    image: "image/summer2026/icon/icon32.png",
+    xUrl: "https://x.com/shudendash_jp",
+    youtubeUrl: "https://www.youtube.com/@shudendashheroes"
+  },
+  {
+    name: "ユミリアの夢案内",
+    image: "image/summer2026/icon/icon33.jpg",
+    xUrl: "https://x.com/YumiliaDreams",
+    youtubeUrl: "https://www.youtube.com/@YumiliaDreams"
+  },
+  {
+    name: "旧雅之",
+    image: "image/summer2026/icon/icon34.jpg",
+    xUrl: "https://x.com/ichijoji_m",
+    youtubeUrl: "https://www.youtube.com/@q_masa"
+  },
+  {
+    name: "Ao（エーオー）",
+    image: "image/summer2026/icon/icon35.png",
+    xUrl: "https://x.com/Ao_eioo",
+    youtubeUrl: "https://www.youtube.com/@Ao%E3%82%A8%E3%83%BC%E3%82%AA%E3%83%BC"
+  },
+  {
+    name: "美桜",
+    image: "image/summer2026/icon/icon36.jpg",
+    xUrl: "https://x.com/AI_mio_AI",
+    youtubeUrl: "https://www.youtube.com/@AI_mio_AI"
+  },
+  {
+    name: "ててつろう",
+    image: "image/summer2026/icon/icon37.png",
+    xUrl: "https://x.com/craftcapitallab",
+    youtubeUrl: "https://www.youtube.com/@%E3%82%B9%E3%82%BF%E3%82%B8%E3%82%AA%E3%81%A6%E3%81%A6%E3%81%A4%E3%82%8D%E3%81%86"
+  },
+  {
+    name: "シロクマペンギン",
+    image: "image/summer2026/icon/icon100.png",
+    xUrl: "https://x.com/RAx0ih0G1PtoqDu",
+    youtubeUrl: "https://youtube.com/@polarbearpenguin9?si=iP8rWXTeZ2dO6qP_"
+  },
+  {
+    name: "nov.audio",
+    image: "image/summer2026/icon/icon39.jpg",
+    xUrl: "https://x.com/ni_ch",
+    youtubeUrl: "https://www.youtube.com/@nov_audio"
+  },
+  {
+    name: "アルパカ紳士",
+    image: "image/summer2026/icon/icon40.jpg",
+    xUrl: "https://x.com/alpaca_shinshi",
+    youtubeUrl: "https://youtube.com/@Lunetta-kawaii"
+  },
+  {
+    name: "ブランシェスカ",
+    image: "image/summer2026/icon/icon41.png",
+    xUrl: "https://x.com/montblanc5815",
+    youtubeUrl: "https://youtube.com/@abnormalbraincrisisrecord"
+  },
+  {
+    name: "kurumi",
+    image: "image/summer2026/icon/icon42.jpg",
+    xUrl: "https://x.com/kurukuru_963963",
+    youtubeUrl: "https://www.youtube.com/channel/UC0DlrQU9L-q2bHeknwX1J-w"
+  },
+  {
+    name: "kan:mi",
+    image: "image/summer2026/icon/icon43.jpg",
+    xUrl: "https://x.com/kan_mi_no9",
+    youtubeUrl: "https://www.youtube.com/@TRIARCHE"
+  },
+  {
+    name: "虎猫G3",
+    image: "image/summer2026/icon/icon44.jpg",
+    xUrl: "https://x.com/toranekog3?s=21&t=6EeKspYvtzQcwqJiqZehlQ",
+    youtubeUrl: "https://www.youtube.com/@ToranekoG3"
+  },
+  {
+    name: "∫varts",
+    image: "image/summer2026/icon/icon45.png",
+    xUrl: "https://x.com/varts_works?s=21",
+    youtubeUrl: "https://youtube.com/@rui_varts?si=ycvybtTLgeBLocHi"
+  },
+  {
+    name: "MoMu & TeaSteam",
+    image: "image/summer2026/icon/icon46.jpg",
+    xUrl: "https://x.com/momuandteasteam",
+    youtubeUrl: "https://www.youtube.com/@momuandteasteam"
+  },
+  {
+    name: "南黒山ソイヤ",
+    image: "image/summer2026/icon/icon47.png",
+    xUrl: "https://x.com/SoiyaChannel",
+    youtubeUrl: "https://youtube.com/@soiyachannel?si=P0yr-3UPWOv8H3lL"
+  },
+  {
+    name: "men's brief factory",
+    image: "image/summer2026/icon/icon48.jpg",
+    xUrl: "https://x.com/mens_brief",
+    youtubeUrl: "https://www.youtube.com/channel/UCup2w1shGJ2if0nE2QAxaZg"
+  },
+  {
+    name: "lazy",
+    image: "image/summer2026/icon/icon49.png",
+    xUrl: "https://x.com/lazy_protocol12",
+    youtubeUrl: "https://youtube.com/@lazy_protocol12"
+  },
+  {
+    name: "Lyric Loom Hanora",
+    image: "image/summer2026/icon/icon50.jpg",
+    xUrl: "https://x.com/lyricloomhanora",
+    youtubeUrl: "https://www.youtube.com/@lyricloomhanora-studio99"
+  },
+  {
+    name: "さとしき",
+    image: "image/summer2026/icon/icon51.png",
+    xUrl: "https://x.com/satoshiki5",
+    youtubeUrl: "https://www.youtube.com/@satoshiki_ai"
+  },
+  {
+    name: "アッシュ・カラノネコ",
+    image: "image/summer2026/icon/icon52.jpg",
+    xUrl: "https://x.com/Ash_freeBGM",
+    youtubeUrl: "https://www.youtube.com/@%E3%82%A2%E3%83%83%E3%82%B7%E3%83%A5_%E3%82%AB%E3%83%A9%E3%83%8E%E3%83%8D%E3%82%B3"
+  },
+  {
+    name: "Pink Shih Tzu (AI CREATOR PONTA)",
+    image: "image/summer2026/icon/icon53.png",
+    xUrl: "https://x.com/pinkshihtzu",
+    youtubeUrl: "#"
+  },
+  {
+    name: "Resonance Diva",
+    image: "image/summer2026/icon/icon54.jpg",
+    xUrl: "https://x.com/resonancediva",
+    youtubeUrl: "https://www.youtube.com/@resonance_diva"
+  },
+  {
+    name: "ねね",
+    image: "image/summer2026/icon/icon55.jpg",
+    xUrl: "https://x.com/anonenonenonene",
+    youtubeUrl: "https://youtube.com/channel/UCbrty3dpwhgQZROiWrMrUEg?si=mT1mHX5ILbNxeqo2"
+  },
+  {
+    name: "noriyang",
+    image: "image/summer2026/icon/icon56.jpeg",
+    xUrl: "https://x.com/noriyang_crypt",
+    youtubeUrl: "https://www.youtube.com/@noriyang0911"
+  },
+  {
+    name: "Sentaku",
+    image: "image/summer2026/icon/icon57.png",
+    xUrl: "https://x.com/sentakusound",
+    youtubeUrl: "https://youtube.com/@sentakusound?si=VWhAOoh4PAiL5iuM"
+  },
+  {
+    name: "Ushizaru",
+    image: "image/summer2026/icon/icon58.jpg",
+    xUrl: "https://x.com/Ushizaru_LAB",
+    youtubeUrl: "https://www.youtube.com/@Ushizaru-il2sw"
+  },
+  {
+    name: "Baumkuchen-Man",
+    image: "image/summer2026/icon/icon59.png",
+    xUrl: "https://x.com/BaumkuchenM",
+    youtubeUrl: "https://www.youtube.com/@baumkuchenman96"
+  },
+  {
+    name: "吉田カゲチカ",
+    image: "image/summer2026/icon/icon60.jpg",
+    xUrl: "https://x.com/kagetica",
+    youtubeUrl: "https://www.youtube.com/@kagetica/"
+  },
+  {
+    name: "CatBeat",
+    image: "image/summer2026/icon/icon61.png",
+    xUrl: "https://x.com/catbeat5656?s=21&t=KKohCF4_huyFwjrfz_ytSA",
+    youtubeUrl: "https://youtube.com/@catbeat-gabu?si=uXaSHa9NUGHI_B7-"
+  },
+  {
+    name: "PO2",
+    image: "image/summer2026/icon/icon100.png",
+    xUrl: "https://x.com/Aegis0_x",
+    youtubeUrl: "#"
+  },
+  {
+    name: "音色(ﾈｲﾛ)",
+    image: "image/summer2026/icon/icon63.jpg",
+    xUrl: "https://x.com/neiro_suno05",
+    youtubeUrl: "#"
+  },
+  {
+    name: "JACKPOT031",
+    image: "image/summer2026/icon/icon64.jpg",
+    xUrl: "https://x.com/JACKPOT031",
+    youtubeUrl: "https://www.youtube.com/@jackpot0031"
+  },
+  {
+    name: "午前七子 wiht 午前七子BAND",
+    image: "image/summer2026/icon/icon65.jpg",
+    xUrl: "https://x.com/ai_imayui",
+    youtubeUrl: "https://www.youtube.com/@MusicDreamGirl"
+  },
+  {
+    name: "夜凪ミミ",
+    image: "image/summer2026/icon/icon66.jpeg",
+    xUrl: "https://x.com/YonagiMimi",
+    youtubeUrl: "https://www.youtube.com/@YonagiMimi"
+  },
+  {
+    name: "e-No.ise",
+    image: "image/summer2026/icon/icon67.png",
+    xUrl: "https://x.com/Bass_Ai_Ai_Ai",
+    youtubeUrl: "#"
+  },
+  {
+    name: "Silfira",
+    image: "image/summer2026/icon/icon68.png",
+    xUrl: "https://x.com/yuiotoartistsub",
+    youtubeUrl: "https://www.youtube.com/@yuiotoartist"
+  },
+  {
+    name: "3-Sync",
+    image: "image/summer2026/icon/icon69.jpg",
+    xUrl: "https://x.com/LuarieTakamiya",
+    youtubeUrl: "https://www.youtube.com/@%E9%AB%98%E5%AE%AE%E3%83%AB%E3%82%A2%E3%83%AA%E3%82%A8-LUA"
+  },
+  {
+    name: "CAGWorks",
+    image: "image/summer2026/icon/icon70.jpg",
+    xUrl: "https://x.com/CagWorks66",
+    youtubeUrl: "https://www.youtube.com/@CAGWorks"
   }
-
 ];
 
 // Formatting Icons
